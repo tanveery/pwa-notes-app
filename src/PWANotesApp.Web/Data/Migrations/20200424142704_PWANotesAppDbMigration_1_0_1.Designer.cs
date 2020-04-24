020fea291c36c84b3b582b895675560f9185b7b5
Tanveer Y <tanveery@users.noreply.github.com>
2020-04-24 19:58:00 +0530

    Added a migration script due to the changes in the entity model.

--- src/PWANotesApp.Web/Data/Migrations/20200424142704_PWANotesAppDbMigration_1_0_1.Designer.cs
+++ src/PWANotesApp.Web/Data/Migrations/20200424142704_PWANotesAppDbMigration_1_0_1.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PWANotesApp.Web.Data;
 
 namespace PWANotesApp.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200424142704_PWANotesAppDbMigration_1_0_1")]
+    partial class PWANotesAppDbMigration_1_0_1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/PWANotesApp.Web/Data/Migrations/20200424142704_PWANotesAppDbMigration_1_0_1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PWANotesApp.Web.Data.Migrations
+{
+    public partial class PWANotesAppDbMigration_1_0_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Owner = table.Column<string>(nullable: true),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    LastUpdatedBy = table.Column<string>(nullable: true),
+                    LastUpdatedDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NoteItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NoteId = table.Column<int>(nullable: false),
+                    Content = table.Column<string>(nullable: true),
+                    Type = table.Column<int>(nullable: false),
+                    Order = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NoteItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NoteItems_Notes_NoteId",
+                        column: x => x.NoteId,
+                        principalTable: "Notes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NoteItems_NoteId",
+                table: "NoteItems",
+                column: "NoteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NoteItems");
+
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
