e7ae582f0a05ff1c554b832a3037f0c5f79367ee
Tanveer Y <tanveery@users.noreply.github.com>
2020-04-25 15:38:10 +0530

    Offline fallback page and image.

--- src/PWANotesApp.Web/wwwroot/service-worker.js
+++ src/PWANotesApp.Web/wwwroot/service-worker.js
@@ -6,29 +6,32 @@
  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-const assetsCache = "AssetsCache-v1";
+const assetsCacheName = 'AssetsCache-v1';
+const dynamicCacheName = 'DynamicCache-v1';
 
 const assetsList = [
-    "/",
-    "/lib/bootstrap/dist/css/bootstrap.min.css",
-    "/lib/fontawesome/css/all.css",
-    "/css/site.css",
-    "/lib/jquery/dist/jquery.min.js",
-    "/lib/bootstrap/dist/js/bootstrap.bundle.min.js",
-    "/lib/fontawesome/js/all.js",
-    "/js/site.js",
-    "/img/favicon.png",
-    "/img/icons/icon-96.png",
-    "/img/icons/icon-128.png",
-    "/img/icons/icon-144.png",
-    "/img/icons/icon-152.png",
-    "/img/icons/icon-192.png",
-    "/img/icons/icon-384.png",
-    "/img/icons/icon-512.png"
+    '/',
+    '/error/offline',
+    '/lib/bootstrap/dist/css/bootstrap.min.css',
+    '/lib/fontawesome/css/all.css',
+    '/css/site.css',
+    '/lib/jquery/dist/jquery.min.js',
+    '/lib/bootstrap/dist/js/bootstrap.bundle.min.js',
+    '/lib/fontawesome/js/all.js',
+    '/js/site.js',
+    '/img/offline-image.png',
+    '/img/favicon.png',
+    '/img/icons/icon-96.png',
+    '/img/icons/icon-128.png',
+    '/img/icons/icon-144.png',
+    '/img/icons/icon-152.png',
+    '/img/icons/icon-192.png',
+    '/img/icons/icon-384.png',
+    '/img/icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(caches.open(assetsCache).then(cache => {
+    event.waitUntil(caches.open(assetsCacheName).then(cache => {
         cache.addAll(assetsList);
     }));
 });
@@ -37,16 +40,32 @@ self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(keys => {
             return Promise.all(keys
-                .filter(key => key !== assetsCache)
+                .filter(key => key !== assetsCacheName && key !== dynamicCacheName)
                 .map(key => caches.delete(key))
             )
         })
     );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(cacheResponse => {
-            return cacheResponse || fetch(event.request);
-        }))
+            return cacheResponse || fetch(event.request).then(serverResponse => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(event.request.url, serverResponse.clone());
+                    return serverResponse;
+                })
+            });
+        }).catch(() => {
+            if (event.request.url.indexOf('.png') > -1 || event.request.url.indexOf('.jpg') > -1 || event.request.url.indexOf('.jpeg') > -1) {
+                return caches.match('/img/offline-image.png');
+            }
+            else if (event.request.url.indexOf('.css') > -1 || event.request.url.indexOf('.js') > -1) {
+                // ignore the non-availability of CSS or JS files.
+            }
+            else {
+                return caches.match('/error/offline');
+            }
+        })
+    );
 });
