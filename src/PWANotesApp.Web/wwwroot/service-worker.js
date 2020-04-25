f2a18d0488495f5b4303bb8fe82f15c743c3578c
Tanveer Y <tanveery@users.noreply.github.com>
2020-04-25 12:42:02 +0530

    Optimized service worker

--- src/PWANotesApp.Web/wwwroot/service-worker.js
+++ src/PWANotesApp.Web/wwwroot/service-worker.js
@@ -1,4 +1,13 @@
-﻿const assetsCache = "AssetsCache";
+﻿/*
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+const assetsCache = "AssetsCache-v1";
+
 const assetsList = [
     "/",
     "/lib/bootstrap/dist/css/bootstrap.min.css",
@@ -17,17 +26,25 @@ const assetsList = [
     "/img/icons/icon-384.png",
     "/img/icons/icon-512.png"
 ];
-self.addEventListener('install', (event) => {
+
+self.addEventListener('install', event => {
     event.waitUntil(caches.open(assetsCache).then(cache => {
         cache.addAll(assetsList);
     }));
 });
 
-self.addEventListener('activate', (event) => {
-    console.log('activate handler', event);
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== assetsCache)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request).then(cacheResponse => {
             return cacheResponse || fetch(event.request);
