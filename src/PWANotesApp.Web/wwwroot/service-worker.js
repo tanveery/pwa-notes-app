c5d01319b116d1bbf8f77bc565d1a65f86be1edc
Tanveer Y <tanveery@users.noreply.github.com>
2020-04-25 06:20:42 +0530

    Cached assets.

--- src/PWANotesApp.Web/wwwroot/service-worker.js
+++ src/PWANotesApp.Web/wwwroot/service-worker.js
@@ -1,5 +1,26 @@
-﻿self.addEventListener('install', (event) => {
-    console.log('install handler', event);
+﻿const assetsCache = "AssetsCache";
+const assetsList = [
+    "/",
+    "/lib/bootstrap/dist/css/bootstrap.min.css",
+    "/lib/fontawesome/css/all.css",
+    "/css/site.css",
+    "/lib/jquery/dist/jquery.min.js",
+    "/lib/bootstrap/dist/js/bootstrap.bundle.min.js",
+    "/lib/fontawesome/js/all.js",
+    "/js/site.js",
+    "/img/favicon.png",
+    "/img/icons/icon-96.png",
+    "/img/icons/icon-128.png",
+    "/img/icons/icon-144.png",
+    "/img/icons/icon-152.png",
+    "/img/icons/icon-192.png",
+    "/img/icons/icon-384.png",
+    "/img/icons/icon-512.png"
+];
+self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(assetsCache).then(cache => {
+        cache.addAll(assetsList);
+    }));
 });
 
 self.addEventListener('activate', (event) => {
@@ -7,5 +28,8 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-    console.log('fetch handler', event);
+    event.respondWith(
+        caches.match(event.request).then(cacheResponse => {
+            return cacheResponse || fetch(event.request);
+        }))
 });
